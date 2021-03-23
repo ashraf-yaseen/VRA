e1839eeff46fadc4084d383196928a8fd72d1450
rachitest <rachit-sabharwal@outlook.com>
2021-03-23 00:19:05 +0000

    update dynamic scraper

--- conference_rec/data_scraper.py
+++ conference_rec/data_scraper.py
@@ -6,12 +6,17 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from chromedriver_py import binary_path
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # %%
 # website 1 is www.wikicfp.com
 base_url = "www.wikicfp.com"
 
 # initiate selenium webdriver
-driver = webdriver.Chrome(executable_path = binary_path)
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path = binary_path, chrome_options = chrome_options)
 driver.get(base_url)
 # %%
