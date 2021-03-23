04b51c63e14bae92074519717995d647dfbd0de6
rachitest <rachit-sabharwal@outlook.com>
2021-03-23 00:06:33 +0000

    update for dynamic scraping

--- conference_rec/data_scraper.py
+++ conference_rec/data_scraper.py
@@ -1,9 +1,17 @@
 # %%
 import requests
-import selenium
 
 import pandas as pd
 
 from bs4 import BeautifulSoup
+from chromedriver_py import binary_path
+from selenium import webdriver
 
+# %%
+# website 1 is www.wikicfp.com
+base_url = "www.wikicfp.com"
+
+# initiate selenium webdriver
+driver = webdriver.Chrome(executable_path = binary_path)
+driver.get(base_url)
 # %%
