6bedd8df64a4ab8dc8116c6abe17bd7737682f37
rachitest <rachit-sabharwal@outlook.com>
2021-03-23 03:43:01 +0000

    add wikicfp scraping info

--- conference_rec/data_scraper.py
+++ conference_rec/data_scraper.py
@@ -10,13 +10,21 @@ from selenium.webdriver.chrome.options import Options
 
 # %%
 # website 1 is www.wikicfp.com
-base_url = "www.wikicfp.com"
+base_url = r"http://www.wikicfp.com/cfp/call?conference=public%20health"
 
 # initiate selenium webdriver
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path = binary_path, chrome_options = chrome_options)
+
+driver = webdriver.Chrome(executable_path = binary_path, options = chrome_options)
 driver.get(base_url)
 # %%
+# TODO: these need to be functions 
+all_links = driver.find_elements_by_xpath("//a[contains(@href, 'event.showcfp?')]")
+for link in all_links:
+    print(link.get_attribute("href"))
+
+pages = driver.find_elements_by_xpath("//td[contains(text(), 'Total of')]")
+print([page.text for page in pages])
