12521064bbcef7d59efab34dca9ae5905ca66f63
rachitest <rachit-sabharwal@outlook.com>
2021-08-08 20:55:14 +0000

    make script functional

--- conference_rec/bm25_rec.py
+++ conference_rec/bm25_rec.py
@@ -11,28 +11,20 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from rank_bm25 import BM25Okapi as bm
 
-# %% (Initialize nltk and lemmatizer)
-nltk.download("punkt")
-nltk.download("averaged_perceptron_tagger")
-nltk.download("wordnet")
-nltk.download("stopwords")
+# %%
+def setLemmatizer():
+    lemmatizer = WordNetLemmatizer()
 
-lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words("english"))
+    return lemmatizer
 
-verb_codes = {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"}
+def processCorpus(data):
+    wiki_token = data.copy()
+    wiki_token = wiki_token[["Conference Title", "WikiCFP Tags", "Conference Description"]]
+    wiki_token.columns = ["title", "tags", "description"]
+    wiki_token.fillna('', inplace = True)
+    wiki_token["soup"] = wiki_token["tags"] + " " + wiki_token["description"]
 
-# %% (read in data)
-wikicfp = dataClean.read_folder("/workspaces/VRA/conference_rec/wikicfp_csv")
-wikicfp = dataClean.unique_confs_per_year(wikicfp)
-wikicfp = dataClean.better_dates(wikicfp)
-
-# %% (pre-process data)
-wiki_token = wikicfp.copy()
-wiki_token = wiki_token[["Conference Title", "WikiCFP Tags", "Conference Description"]]
-wiki_token.columns = ["title", "tags", "description"]
-wiki_token.fillna('', inplace = True)
-wiki_token["soup"] = wiki_token["tags"] + " " + wiki_token["description"]
+    return wiki_token
 
 def preprocess_sentences(text):
     text = text.lower()
@@ -55,27 +47,62 @@ def preprocess_sentences(text):
     finalsent = finalsent.replace("'ll", " will")
     finalsent = finalsent.replace("'ve", " have")
     finalsent = finalsent.replace("'d", " would")
+    
     return finalsent
 
-with mp.Pool(mp.cpu_count()) as pool:
-    wiki_token["processed_soup"] = pool.map(preprocess_sentences, wiki_token["soup"])
+def multiprocessApply(function, corpus, data_col, new_col):
+    with mp.Pool(mp.cpu_count()) as pool:
+        corpus[new_col] = pool.map(function, corpus[data_col])
 
-# %% (create bm25 class)
-wiki_bm = wiki_token["processed_soup"].copy().to_list()
-wiki_bm_token = [doc.split(" ") for doc in wiki_bm]
+    return corpus
 
-bm25 = bm(wiki_bm_token)
+def createBMObject(corpus, data_col):
+    wiki_bm = corpus[data_col].copy().to_list()
+    wiki_bm_token = [doc.split(" ") for doc in wiki_bm]
 
-# %% (run query)
-query = "AMIA 2022 Informatics Summit: From discovering innovative methods to learning from exciting real-world applications, AMIA 2022 Informatics Summit attendees will experience the full range of cutting-edge work in translational informatics and clinical data science from inception to implementation. This conference is the ideal setting for researchers, educators, data scientists, software developers and analysts, students, and industry professionals. The size of the conference makes it ideal for developing meaningful new connections and partnerships while learning practical advice to solve real-world challenges. New to the AMIA 2022 Informatics Summit, we have expanded upon the previous Informatics Implementation track to include it as a new theme: Applied Informatics. In addition to selecting one of the three core Programmatic Tracks (Clinical Research Informatics, Data Science, Translational Bioinformatics), authors/presenters can also choose to designate their submission as part of the Applied Informatics theme to highlight the crucial application and implementation focus of their work. This is first time the Informatics Summit convenes outside of San Francisco. We are confident Chicago will bring new collaborations and connections. We look forward to receiving your submissions"
-query = preprocess_sentences(query)
+    bm25 = bm(wiki_bm_token)
 
-token_query = query.split(" ")
+    return bm25
 
-doc_scores = bm25.get_scores(token_query)
+def getBM25Ranks(processing_func, query, model):
+    query = processing_func(query)
+    token_query = query.split(" ")
+    doc_scores = model.get_scores(token_query)
 
-# %% (retrieve results)
-results = np.argsort(doc_scores)[-10:]
-results = np.flip(results)
+    return doc_scores
 
-final_recs = wikicfp.loc[wikicfp.index[results]]
+def getRecs(model_scores, top_n, corpus):
+    results = np.argsort(model_scores)[-top_n:]
+    results = np.flip(results)
+
+    final_recs = corpus.loc[corpus.index[results]]
+
+    return final_recs
+
+if __name__ == "__main__":
+    nltk.download("punkt")
+    nltk.download("averaged_perceptron_tagger")
+    nltk.download("wordnet")
+    nltk.download("stopwords")
+
+    stop_words = set(stopwords.words("english"))
+
+    verb_codes = {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"}
+
+    lemmatizer = setLemmatizer()
+
+    wikicfp = dataClean.read_folder("/workspaces/VRA/conference_rec/wikicfp_csv")
+    wikicfp = dataClean.unique_confs_per_year(wikicfp)
+    wikicfp = dataClean.better_dates(wikicfp)
+
+    wiki_token = processCorpus(wikicfp)
+
+    wiki_token = multiprocessApply(preprocess_sentences, wiki_token, "soup", "processed_soup")
+
+    bm25_model = createBMObject(wiki_token, "processed_soup")
+
+    query = "AMIA 2022 Informatics Summit: From discovering innovative methods to learning from exciting real-world applications, AMIA 2022 Informatics Summit attendees will experience the full range of cutting-edge work in translational informatics and clinical data science from inception to implementation. This conference is the ideal setting for researchers, educators, data scientists, software developers and analysts, students, and industry professionals. The size of the conference makes it ideal for developing meaningful new connections and partnerships while learning practical advice to solve real-world challenges. New to the AMIA 2022 Informatics Summit, we have expanded upon the previous Informatics Implementation track to include it as a new theme: Applied Informatics. In addition to selecting one of the three core Programmatic Tracks (Clinical Research Informatics, Data Science, Translational Bioinformatics), authors/presenters can also choose to designate their submission as part of the Applied Informatics theme to highlight the crucial application and implementation focus of their work. This is first time the Informatics Summit convenes outside of San Francisco. We are confident Chicago will bring new collaborations and connections. We look forward to receiving your submissions"
+
+    query_scores = getBM25Ranks(preprocess_sentences, query, bm25_model)
+
+    recs = getRecs(query_scores, 10, wikicfp)
